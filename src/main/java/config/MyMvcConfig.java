a3c3424c5ea05cc8e174040e6d2a4006191a62a6
pumzx <1050098245@qq.com>
2018-10-24 13:29:37 +0800

    spring mvc 高级用法

--- src/main/java/config/MyMvcConfig.java
+++ src/main/java/config/MyMvcConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.*;
